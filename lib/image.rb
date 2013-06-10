26bb75f812508d871a9433837787668a2cd9b5f0
dariodaprile <dario.daprile@gmail.com>
2013-06-10 18:53:08 +0100

    3rd test

--- lib/image.rb
+++ lib/image.rb
@@ -1,10 +1,23 @@
 class Image
-  attr_reader :width, :height
 
-  def initialize (m,n)
-    @width = m
-    @height = n
+  attr_accessor :width, :height, :colour, :image
+
+  def initialize (width, height, colour = "O")
+    @width = width
+    @height = height
+    @colour = colour
   end
 
+  def to_image
+    @pixel_array = Array.new(@width * @height)
+    @pixel_array_of_colours = @pixel_array.map{|i| @colour}
+    @images = @pixel_array_of_colours.join.scan(/.{5}/).join("\n")
+  end
+
+  def change_color(x,y,colour)
+    # a = to_image.split(/\n/)
+    # b = a[y-1][x-1]
+    # b.gsub!("O",colour)
+  end
 
 end
