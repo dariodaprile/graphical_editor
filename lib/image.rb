18c6423d12a7eb1769c3c3baddf95bb5312957c2
dariodaprile <dario.daprile@gmail.com>
2013-06-11 14:09:36 +0100

    created test for horizontal and vertical segments

--- lib/image.rb
+++ lib/image.rb
@@ -2,9 +2,7 @@ class Image
   attr_reader :height, :width, :colour
 
   def initialize(width, height, colour = "O")
-    @width = width
-    @height = height
-    @colour = colour
+    @width, @height, @colour = width, height, colour
     @canvas = Array.new(height){Array.new(width){colour}}
   end
 
@@ -12,10 +10,36 @@ class Image
     @canvas.map{|i| i.join }.join("\n")
   end
 
-  def colour_pixel(x,y,colour)
-    @canvas[y-1] [x-1] = colour
+  def colour_pixel(x,y, colour = "O")
+    @canvas[y-1][x-1] = colour
   end
 
+  def colour_vertical_segment(x,y1,y2,colour)
+    (y1..y2).each { |y| colour_pixel(x,y,colour)}
+  end
 
+  def colour_horizontal_segment(x1,x2,y,colour)
+    (x1..x2).each { |x| colour_pixel(x,y,colour)}
+  end
+
+  def fill(x,y,new_colour)
+    original_colour = colour_pixel(x,y,colour)
+    recursive_fill(x,y,new_colour,original_colour)
+  end
+
+  # private
+
+  def recursive_fill(x,y,new_colour,original_colour)
+    colour_pixel(x,y,new_colour)
+    adjacent(x,y).each do |px|
+      recursive_fillh(x[px], y[px], new_colour, original_colour)
+      if colour(x[px], y[px]) == original_colour
+      end
+    end
+  end
+
+  def adjacent (x,y)
+    (y-1..y+1).map { |ay| (x-1..x+1).map { |ax| [ax, ay] } }
+  end
 
 end
--- test/test_image.rb
+++ test/test_image.rb
@@ -4,29 +4,43 @@ require '../lib/image'
 
 class ImageTest < MiniTest::Unit::TestCase
 
-  def test_set_pixel_width_and_height
+  def setup
     @image = Image.new(5,6)
+  end
+
+  def test_set_pixel_width_and_height
     assert_equal 5, @image.width
     assert_equal 6, @image.height
   end
 
   def test_set_default_colour
-    @image = Image.new(5,6)
     assert_equal "O", @image.colour
   end
 
   def test_each_pixel_is_white_by_default
-    @image = Image.new(5,6)
     assert_equal "OOOOO\nOOOOO\nOOOOO\nOOOOO\nOOOOO\nOOOOO", @image.to_image
   end
 
-  def test_color_a_specific_pixel
-    # prepare the object
-    @image = Image.new(5,6)
-    # do the action
+  def test_colour_a_specific_pixel
     @image.colour_pixel(2,3,"A")
-    # check the result
     assert_equal "OOOOO\nOOOOO\nOAOOO\nOOOOO\nOOOOO\nOOOOO", @image.to_image
+  end
+
+  def test_colour_a_vertical_segment
+    @image.colour_vertical_segment(2,3,4,"W")
+    assert_equal "OOOOO\nOOOOO\nOWOOO\nOWOOO\nOOOOO\nOOOOO", @image.to_image
+  end
+
+  def test_colour_a_horizontal_segment
+    @image.colour_horizontal_segment(3,4,2,"Z")
+    assert_equal "OOOOO\nOOZZO\nOOOOO\nOOOOO\nOOOOO\nOOOOO", @image.to_image
+  end
+
+  def test_fill_the_region
+    @image.colour_vertical_segment(2,3,4,"W")
+    @image.colour_horizontal_segment(3,4,2,"Z")
+    @image.fill(3,3,"J")
+    assert_equal "JJJJJ\nJJZZJ\nJWJJJ\nJWJJJ\nJJJJJ\nJJJJJ", @image.to_image
   end
 
 
