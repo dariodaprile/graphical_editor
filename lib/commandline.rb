d78b79acc579d77bb605ce5b966041ce5e62d96e
dariodaprile <dario.daprile@gmail.com>
2013-06-13 14:19:37 +0100

    working on commandline

--- lib/commandline.rb
+++ lib/commandline.rb
@@ -1,22 +1,44 @@
 require_relative 'image'
 
-
 class CommandLine
 
   attr_reader :image
 
-  puts "Type 'I' followed by the width and height (integers) to create an image >"
-  input = gets.chomp
+  COMMANDS = {
+    /X/ => :quit,
+    /S/ => :show,
+    /I/ => :new_image,
+    /C/ => :clear_image,
+    /L/ => :colour_pixel,
+    /V/ => :colour_column,
+    /H/ => :colour_row,
+    /F/ => :colour_region,
+    /.*/ => :unrecognised,
+  }
 
-  if input[/I/]
-    a = input.split(' ')
-    width, height = a[1].to_i, a[2].to_i
-    puts "variable created"
-    @image  = Image.new(width, height)
-    puts "great we have the image"
-    elsecommandline
-      puts "The first input should be I, by Charlotte."
-    end
+  def execute (input)
+    puts "Welcome to the graphical editor!"
+    print "> "
+
+    regex = COMMANDS.keys.find{|regex| regex =~ input }
+    matches = regex.match(input).to_a
+    command = COMMANDS[regex]
+    arguments = matches.slice(1, matches.length - 1)
+    [command, *arguments]
+  end
+
+  # puts "Type 'I' followed by the width and height (integers) to create an image >"
+  # input = gets.chomp
+
+  # if input[/I/]
+  #   a = input.split(' ')
+  #   width, height = a[1].to_i, a[2].to_i
+  #   puts "variable created"
+  #   @image  = Image.new(width, height)
+  #   puts "great we have the image"
+  #   elsecommandline
+  #     puts "The first input should be I, by Charlotte."
+  #   end
 
 # def run
 #     image.show
--- test/test_commandline.rb
+++ test/test_commandline.rb
@@ -6,23 +6,29 @@ require 'mocha/setup'
 
 
 class CommandLineTest < Test::Unit::TestCase
+
   def setup
     @commandline = CommandLine.new
   end
 
-  def test_user_can_create_image
-    #prepare the object
-    commandline = CommandLine.new
-    mock_image = Minitest::Mock.new
-    mock_image.expect(:show)
-    commandline.stub :image, mock_image do
-      commandline.run
-    end
-    mock_image.verify
-    #do the action
-    #check the result
-
+  def test_commandline_can_execute_exit_command
+    @commandline.expects(:quit)
+    @commandline.execute "X"
   end
+
+  # def test_user_can_create_image
+  #   #prepare the object
+  #   commandline = CommandLine.new
+  #   mock_image = Minitest::Mock.new
+  #   mock_image.expect(:show)
+  #   commandline.stub :image, mock_image do
+  #     commandline.run
+  #   end
+  #   mock_image.verify
+  #   #do the action
+  #   #check the result
+
+  # end
 
 
 end
